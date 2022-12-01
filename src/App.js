0d1456698e7c666db8f54b42fa52886a2c3bb789
Daniel Raad <daniel_raad@hotmail.co.uk>
2022-12-01 10:37:24 +0000

    wip6

--- src/App.js
+++ src/App.js
@@ -1,36 +1,26 @@
 // import logo from './logo.svg';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+// } from "react-router-dom";
 import NavbarExternal from './Components/NavbarExternal';
-import ForYou from './Pages/ClientApp/ForYou';
+// import ForYou from './Pages/ClientApp/ForYou';
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header"> */}
       <NavbarExternal />
-      <Router>
+      {/* <Router>
         <div>
-          {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
           <Switch>
-            {/* <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/live-event">
-            <LiveEvent />
-          </Route> */}
             <Route path="/">
               <ForYou />
             </Route>
           </Switch>
         </div>
-      </Router>
-      {/* </header> */}
+      </Router> */}
     </div>
   );
 }
